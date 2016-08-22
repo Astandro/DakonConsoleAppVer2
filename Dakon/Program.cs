30d2a8fde02b56d1033ff13bc105e47002ae0d70
Astandro <astandro.tc@gmail.com>
2016-08-22 20:59:37 +0700

    Updated Major Logic
    Major update for entire dakon logic to match the rules

--- Dakon/Program.cs
+++ Dakon/Program.cs
@@ -7,21 +7,26 @@ namespace Dakon
 {
     class Program
     {
-        //Referensi : https://www.youtube.com/watch?v=zAGYhT05AIc
-        //Game contoh : http://dakon-the-game.appspot.com/
-        //Kondisi-kondisi permainan dakon :
-        //Ronde 1
-        // 1. berjalan secara berputar searah jarum jam
-        // 2. deposit biji pada store house (lubang besar) milik sendiri bukan milik musuh
-        // 3. jika biji di tangan habis pada lubang yang terdapat biji, maka perputaran dilanjutkan dengan mengambil seluruh biji pada lubang tersebut
-        // 4. jika biji di tangan habis pada store house (lubang besar) milik sendiri, maka player tersebut mendapat jatah giliran lagi
-        // 5. jika biji di tangan habis pada lubang kosong milik musuh, maka giliran pemain tersebut berakhir
-        // 6. jika biji di tangan habis pada lubang kosong milik sendiri, maka ambil seluruh biji pada lubang milik musuh yang berseberangan
-        // 
+        //Aplikasi Console Dakon - Bukalapak Test
+
+        //Referensi : https://www.youtube.com/watch?v=zAGYhT05AIc 
+        //Referensi 2 : http://www.expat.or.id/info/congklakinstructions.html
+
+        //Kondisi-kondisi permainan dakon : Ronde 1
+
+        //1. berjalan secara berputar searah jarum jam
+        //2. deposit biji pada store house (lubang besar) milik sendiri bukan milik musuh
+        //3. jika biji di tangan habis pada lubang yang terdapat biji, maka perputaran dilanjutkan dengan mengambil seluruh biji pada lubang tersebut
+        //4. jika biji di tangan habis pada store house (lubang besar) milik sendiri, maka player tersebut mendapat jatah giliran lagi
+        //5. jika biji di tangan habis pada lubang kosong milik musuh, maka giliran pemain tersebut berakhir
+        //6. jika biji di tangan habis pada lubang kosong milik sendiri, maka ambil seluruh biji pada lubang tersebut ditambah milik musuh yang berseberangan
+        //7. Permainan berakhir jika salah satu pemain kehabisan biji pada seluruh lubang miliknya (kalah jalan)
+
         //Ronde 2
-        //1. sama seperti ronde 1, hanya saja sebagai permulaan, seluruh isi storehouse didistribusikan ke masing2 lubang, 
-        //   jika tidak cukup untuk memenuhi 1 lubang, maka sisanya dibiarkan di storehouse
-        //2. jika ada lubang yang kosong/tidak terisi, maka lubang tersebut dinyatakan burnt/tidak bisa digunakan
+
+        //1. sama seperti ronde 1, hanya saja sebagai permulaan, seluruh isi storehouse didistribusikan ke masing2 lubang, jika tidak cukup untuk memenuhi 1 lubang, maka sisanya dibagikan secara merata pada lubang sisanya.
+        //2. jika ada lubang yang tidak terisi penuh (7 biji), maka lubang tersebut dinyatakan burnt (tidak bisa dilewati musuh, biji di dalamnya tidak boleh diambil, tidak bisa ditembak)
+
 
         static PapanDakon papanDakon1;
         static PointerTangan tangan;
@@ -50,11 +55,17 @@ namespace Dakon
                     geserPosisiTangan();
                 }
 
-                //jika posisi berhenti bukan di storehouse, maka giliran pemain tersebut berakhir
-                if (!papanDakon1.listLubang[tangan.indexPosisi].isStoreHouse)
-                    switchPlayer();
+                //jika tidak ada player yang kalah jalan
+                if (!isP1KalahJalan() && !isP2KalahJalan())
+                {
+                    //jika posisi berhenti bukan di storehouse, maka giliran pemain tersebut berakhir
+                    if (!papanDakon1.listLubang[tangan.indexPosisi].isStoreHouse)
+                        switchPlayer();
+                    else
+                        printGetAdditionlRoundMessage();  //mendapat jatah 1 giliran lagi
+                }
                 else
-                    printGetAdditionlRoundMessage();  //mendapat jatah 1 giliran lagi
+                    printKalahJalanMessage();
 
                 papanDakon1.printPapanDakon();
             }
@@ -62,11 +73,10 @@ namespace Dakon
             //Ambil sisa biji milik pemenang & masukkan ke storehousenya
             if (isP1KalahJalan())
                 papanDakon1.listLubang[0].marblesCount += getSisaBijiP2();
-            else
+            else if (isP2KalahJalan())
                 papanDakon1.listLubang[8].marblesCount += getSisaBijiP1();
-
-            //Inisialisasi Player yang berhak memulai duluan di ronde ke 2
-            setSecondRoundFirstPlayer();
+            else//Inisialisasi Player yang berhak memulai duluan di ronde ke 2
+                setSecondRoundFirstPlayer();
 
             //Inisialisasi ronde ke 2
             Boolean isSecondRound = papanDakon1.startSecondRound();
@@ -78,7 +88,7 @@ namespace Dakon
             papanDakon1.printPapanDakon();
 
             //Looping giliran pada Ronde 2
-            while (papanDakon1.getTotalMarblesInStoreHouses() != 98 && isSecondRound)
+            while (!isP1KalahJalan() && !isP2KalahJalan() && isSecondRound)
             {
                 //Get user input
                 pilihDanAmbilIsiLubang();
@@ -89,11 +99,17 @@ namespace Dakon
                     geserPosisiTangan();
                 }
 
-                //jika posisi berhenti bukan di storehouse, maka giliran pemain tersebut berakhir
-                if (!papanDakon1.listLubang[tangan.indexPosisi].isStoreHouse)
-                    switchPlayer();
+                //jika tidak ada player yang kalah jalan
+                if (!isP1KalahJalan() && !isP2KalahJalan())
+                {
+                    //jika posisi berhenti bukan di storehouse, maka giliran pemain tersebut berakhir
+                    if (!papanDakon1.listLubang[tangan.indexPosisi].isStoreHouse)
+                        switchPlayer();
+                    else
+                        printGetAdditionlRoundMessage();  //mendapat jatah 1 giliran lagi
+                }
                 else
-                    printGetAdditionlRoundMessage();  //mendapat jatah 1 giliran lagi
+                    printKalahJalanMessage();
 
                 papanDakon1.printPapanDakon();
             }
@@ -116,6 +132,14 @@ namespace Dakon
             Console.WriteLine("Permainan dimenangkan oleh "+winner);
         }
 
+        public static void printKalahJalanMessage()
+        {
+            if(isP1KalahJalan())
+                Console.WriteLine("Player 1 mengalami kalah jalan, Player 2 menang jalan");
+            else
+                Console.WriteLine("Player 2 mengalami kalah jalan, Player 1 menang jalan");
+        }
+
         public static void printGetAdditionlRoundMessage()
         {
             Console.WriteLine(tangan.playerInTurn + " mendapatkan jatah giliran lagi karena berhenti di storehouse");
@@ -131,8 +155,6 @@ namespace Dakon
 
         public static void pilihDanAmbilIsiLubang()
         {
-            Console.Write("\nGiliran " + tangan.playerInTurn + ", Silahkan pilih posisi (1-7) : ");
-
             //Player 1 memilih house yang akan digunakan sebagai titik awal giliran
             tangan.indexPosisi = getHousePilihanUser(tangan.playerInTurn);
 
@@ -152,21 +174,37 @@ namespace Dakon
             string input;
             while (!isValidInput)
             {
+                Console.Write("\nGiliran " + tangan.playerInTurn + ", Silahkan pilih posisi (1-7) : ");
                 input = Console.ReadLine();
                 isNumeric = int.TryParse(input, out houseNum);
 
                 if (isNumeric && houseNum >= 0 && houseNum < 8)
-                    isValidInput = true;
+                {
+                    if (playerInThisTurn == "Player 2")
+                        houseNum += 8;
+
+                    if (papanDakon1.listLubang[houseNum].isEmpty)
+                    {
+                        Console.WriteLine("Lubang yang anda pilih kosong, silahkan pilih yang lain");
+                    }
+                    else
+                    {
+                        if (papanDakon1.listLubang[houseNum].isBurnt)
+                        {
+                            Console.WriteLine("Lubang yang anda pilih adalah lubang ngacang, silahkan pilih yang lain");
+                        }
+                        else
+                            isValidInput = true;
+                    }
+                    
+                }
                 else
                 {
                     Console.WriteLine("Input tidak valid!");
                 }
             }
 
-            if (playerInThisTurn == "Player 1")
-                return houseNum;
-            else
-                return houseNum + 8;
+            return houseNum;
         }
 
         public static void geserPosisiTangan()
@@ -185,29 +223,35 @@ namespace Dakon
                 tangan.marblesDiTangan--;
 
                 // 3. jika biji di tangan habis pada lubang yang terdapat biji, maka perputaran dilanjutkan dengan mengambil seluruh biji pada lubang tersebut
-                if (tangan.marblesDiTangan == 0 && !papanDakon1.listLubang[tangan.indexPosisi].isEmpty && !papanDakon1.listLubang[tangan.indexPosisi].isStoreHouse)
+                if (tangan.marblesDiTangan == 0 && !papanDakon1.listLubang[tangan.indexPosisi].isEmpty && !papanDakon1.listLubang[tangan.indexPosisi].isStoreHouse && !papanDakon1.listLubang[tangan.indexPosisi].isBurnt)
                 {
                     tangan.marblesDiTangan = papanDakon1.listLubang[tangan.indexPosisi].marblesCount + 1;
                     papanDakon1.listLubang[tangan.indexPosisi].marblesCount = 0;
                     papanDakon1.listLubang[tangan.indexPosisi].isEmpty = true;
                 }
                 // 6. jika biji di tangan habis pada lubang kosong milik sendiri, maka ambil seluruh biji pada lubang milik musuh yang berseberangan
-                else if (tangan.marblesDiTangan == 0 && papanDakon1.listLubang[tangan.indexPosisi].isEmpty && !papanDakon1.listLubang[tangan.indexPosisi].isStoreHouse && papanDakon1.listLubang[tangan.indexPosisi].owner == tangan.playerInTurn)
+                else if (tangan.marblesDiTangan == 0 && papanDakon1.listLubang[tangan.indexPosisi].isEmpty && !papanDakon1.listLubang[tangan.indexPosisi].isStoreHouse && papanDakon1.listLubang[tangan.indexPosisi].owner == tangan.playerInTurn && !papanDakon1.listLubang[tangan.indexPosisi].isBurnt)
                 {
-                    papanDakon1.listLubang[tangan.indexPosisi].marblesCount = 1;
-                    papanDakon1.listLubang[tangan.indexPosisi].isEmpty = false;
-
-                    if (tangan.playerInTurn == "Player 1")
+                    if (tangan.playerInTurn == "Player 1" && !papanDakon1.listLubang[16 - tangan.indexPosisi].isBurnt)
                     {
+                        papanDakon1.listLubang[tangan.indexPosisi].marblesCount = 1;
+                        papanDakon1.listLubang[tangan.indexPosisi].isEmpty = false;
                         papanDakon1.listLubang[8].marblesCount += papanDakon1.listLubang[16 - tangan.indexPosisi].marblesCount;
                         papanDakon1.listLubang[16 - tangan.indexPosisi].marblesCount = 0;
                         papanDakon1.listLubang[16 - tangan.indexPosisi].isEmpty = true;
                     }
-                    else
+                    else if (tangan.playerInTurn == "Player 2" && !papanDakon1.listLubang[7 - (tangan.indexPosisi - 9)].isBurnt)
                     {
+                        papanDakon1.listLubang[tangan.indexPosisi].marblesCount = 1;
+                        papanDakon1.listLubang[tangan.indexPosisi].isEmpty = false;
                         papanDakon1.listLubang[0].marblesCount += papanDakon1.listLubang[7 - (tangan.indexPosisi - 9)].marblesCount;
                         papanDakon1.listLubang[7 - (tangan.indexPosisi - 9)].marblesCount = 0;
                         papanDakon1.listLubang[7 - (tangan.indexPosisi - 9)].isEmpty = true;
+                    }
+                    else
+                    {
+                        papanDakon1.listLubang[tangan.indexPosisi].marblesCount++;
+                        papanDakon1.listLubang[tangan.indexPosisi].isEmpty = false;
                     }
                 }
                 // 1. berjalan secara berputar searah jarum jam
